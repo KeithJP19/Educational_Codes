243f8e50501cecb5efd94f2243defbdb281aa2cc
KeithJP19 <150800104+KeithJP19@users.noreply.github.com>
2024-10-10 21:33:35 +0800

    Create HelloWorld.cpp
    A Hello world in C++ that greets the users with their name.

---
+++ Codes_C++/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//A simple Hello world code
+
+int main(void)
+{
+  string name;
+cout << "Enter your Name: " ;
+cin >> name;
+cout << "Hello " << name << " to Coding World!" << endl;
+
+return 0;
+}
